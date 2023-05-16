d629f791fd9e00096ab990f3b9aae102a53fb8cf
mkmulvi30 <98337534+mkmulvi30@users.noreply.github.com>
2023-05-16 14:36:04 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
+function Pause500Clear () {
+    basic.pause(500)
+    basic.clearScreen()
+}
 function readLightLevel () {
     basic.pause(500)
     basic.showNumber(input.lightLevel())
@@ -8,12 +12,21 @@ input.onButtonPressed(Button.A, function () {
     readLightLevel()
 })
 input.onButtonPressed(Button.B, function () {
-    if (input.lightLevel() <= 75) {
+    if (input.lightLevel() == 0) {
+        basic.showString("NULL")
+        basic.pause(500)
+        basic.clearScreen()
+        basic.showIcon(IconNames.Sad)
+        basic.clearScreen()
+        basic.pause(500)
+        readLightLevel()
+    } else if (input.lightLevel() <= 75) {
         basic.showString("Hello?")
         basic.pause(500)
         basic.clearScreen()
         basic.showIcon(IconNames.Sad)
         basic.clearScreen()
+        basic.pause(500)
         readLightLevel()
     } else if (input.lightLevel() > 75 && input.lightLevel() <= 110) {
         basic.showString("Dim")
@@ -21,6 +34,7 @@ input.onButtonPressed(Button.B, function () {
         basic.clearScreen()
         basic.showIcon(IconNames.Yes)
         basic.clearScreen()
+        basic.pause(500)
         readLightLevel()
     } else if (input.lightLevel() > 110 && input.lightLevel() <= 200) {
         basic.showString("Perfect")
@@ -28,6 +42,7 @@ input.onButtonPressed(Button.B, function () {
         basic.clearScreen()
         basic.showIcon(IconNames.Happy)
         basic.clearScreen()
+        basic.pause(500)
         readLightLevel()
     } else if (input.lightLevel() > 200) {
         basic.showString("Yowsa!")
@@ -35,6 +50,11 @@ input.onButtonPressed(Button.B, function () {
         basic.clearScreen()
         basic.showIcon(IconNames.Target)
         basic.clearScreen()
+        basic.pause(500)
         readLightLevel()
     }
 })
+function Clear500Pause () {
+    basic.clearScreen()
+    basic.pause(500)
+}
