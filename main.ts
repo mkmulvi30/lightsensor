78fbf53bc6881e6ffae8e12b9de1c9c6e76e0f03
mkmulvi30 <98337534+mkmulvi30@users.noreply.github.com>
2023-05-16 08:01:32 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,23 +4,26 @@ function readLightLevel () {
     basic.pause(1000)
     basic.clearScreen()
 }
-basic.forever(function () {
+input.onButtonPressed(Button.A, function () {
+    readLightLevel()
+})
+input.onButtonPressed(Button.B, function () {
     if (input.lightLevel() <= 75) {
-        basic.showString("Too Dark!")
+        basic.showString("Hello?")
         basic.pause(500)
         basic.clearScreen()
         basic.showIcon(IconNames.Sad)
         basic.clearScreen()
         readLightLevel()
     } else if (input.lightLevel() > 75 && input.lightLevel() <= 110) {
-        basic.showString("Let there be light")
+        basic.showString("Dim")
         basic.pause(500)
         basic.clearScreen()
         basic.showIcon(IconNames.Yes)
         basic.clearScreen()
         readLightLevel()
     } else if (input.lightLevel() > 110 && input.lightLevel() <= 200) {
-        basic.showString("Glimmer of Hope")
+        basic.showString("Perfect")
         basic.pause(500)
         basic.clearScreen()
         basic.showIcon(IconNames.Happy)
